076cf1247a9d22ee223d44240ffadfa68317d007
jiseong0612 <hth5545@gmail.com>
2023-04-15 23:43:29 +0900

    스프링 핵심 원리 이해2 - 스프링으로 전환하기

--- src/main/java/hello/core/order/OrderApp.java
+++ src/main/java/hello/core/order/OrderApp.java
@@ -1,5 +1,8 @@
 package hello.core.order;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
@@ -8,17 +11,20 @@ import hello.core.member.MemberService;
 public class OrderApp {
 	public static void main(String[] args) {
 		
-		AppConfig appConfig = new AppConfig();
+//		AppConfig appConfig = new AppConfig();
+//		MemberService memberService = appConfig.memberService();
+//		OrderService orderService = appConfig.orderService();
+		ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 		
-		MemberService memberService = appConfig.memberService();
-		OrderService orderService = appConfig.orderService();
+		MemberService memberService = ac.getBean("memberService", MemberService.class);
+		OrderService orderService = ac.getBean("orderService", OrderService.class);
 		Long memberId = 1L;
 		
 		Member member = new Member(memberId, "memberA", Grade.VIP);
 		
 		memberService.join(member);
 		
-		Order order = orderService.createOrder(memberId, "itemA", 10000);
+		Order order = orderService.createOrder(memberId, "itemA", 20000);
 		System.out.println(order);
 	}
 }
