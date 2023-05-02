558c54eb4df3e2f5a1f5919439cef83dd5bec475
jiseong0612 <hth5545@gmail.com>
2023-05-03 02:05:20 +0900

    스프링 MVC 1편 - 백엔드 웹 개발 핵심 기술 -스프링 MVC - 웹 페이지 만들기 :상품 도메인 개발 프로젝트 초기 세팅

--- src/main/java/hello/core/member/MemberRepository.java
+++
@@ -1,6 +0,0 @@
-package hello.core.member;
-
-public interface MemberRepository {
-	void join(Member member);
-	Member findById(Long memberId);
-}
--- src/test/java/hello/core/singleton/MyIncludeComponent.java
+++
@@ -1,5 +0,0 @@
-package hello.core.singleton;
-
-public @interface MyIncludeComponent {
-
-}
