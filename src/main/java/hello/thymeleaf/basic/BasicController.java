2cf16b84c01e35466658582f191a541dbc90c9a4
jiseong0612 <hth5545@gmail.com>
2023-06-20 01:06:28 +0900

    스프링 부트 배너 적용, 스프링 MVC 2편 - 백엔드 웹 개발 활용 기술-자바스크립트 인라인

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,6 +1,8 @@
 package hello.thymeleaf.basic;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -12,39 +14,73 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.Data;
+
 @Controller
 @RequestMapping("/basic")
 public class BasicController {
 
 	@GetMapping("/text-basic")
 	public String textBasic(Model model) {
-		
+
 		model.addAttribute("data", "<b>Hello</b> Spring~!");
-		
+
 		Map<String, String> userMap = new HashMap<>();
 		userMap.put("userA", "aaa");
 		userMap.put("userB", "bbb");
-		
+
 		model.addAttribute("userMap", userMap);
-		
+
 		return "basic/text-basic";
 	}
-	
+
 	@GetMapping("/basic-objects")
 	public String basicObjects(HttpSession session, HttpServletRequest req) {
 		HttpSession session2 = req.getSession();
 		session.setAttribute("sessionData", "session1");
-		session2.setAttribute("session2", "session2"); 
-		
+		session2.setAttribute("session2", "session2");
+
 		System.out.println(session == session2);
-		
-		return "basic/basic-objects"; 	
+
+		return "basic/basic-objects";
 	}
-	
+
+	@GetMapping("/block")
+	public String block(Model model) {
+		addUsers(model);
+		return "basic/block";
+	}
+
+	@GetMapping("/javascript")
+	public String javascript(Model model) {
+		model.addAttribute("user", new User("userA", 10));
+		addUsers(model);
+		return "basic/javascript";
+	}
+
 	@Component("helloBean")
 	static class HelloBean {
-		public String hello (String data) {
+		public String hello(String data) {
 			return "hello " + data;
 		}
+	}
+
+	@Data
+	static class User {
+		private String username;
+		private int age;
+
+		public User(String username, int age) {
+			this.username = username;
+			this.age = age;
+		}
+	}
+
+	private void addUsers(Model model) {
+		List<User> list = new ArrayList<>();
+		list.add(new User("userA", 10));
+		list.add(new User("userB", 20));
+		list.add(new User("userC", 30));
+		model.addAttribute("users", list);
 	}
 }
